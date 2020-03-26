4d37f6a704a7162441366a7647d9a451f1f31a29
olorondu-emeka <oloronduchukwuemeka@gmail.com>
2020-03-26 21:33:51 +0100

    added business logic to PostService & PostController, and updated PostRepository

--- src/main/java/com/emeka/blogspringboot/controllers/PostController.java
+++ src/main/java/com/emeka/blogspringboot/controllers/PostController.java
@@ -1,7 +1,40 @@
 package com.emeka.blogspringboot.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.emeka.blogspringboot.models.Post;
+import com.emeka.blogspringboot.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class PostController {
+    @Autowired
+    private PostService postService;
+
+    @GetMapping("/posts")
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/posts/{authorId}")
+    public List<Post> getPostsByAuthor(@PathVariable int authorId) {
+        return postService.getPostsByAuthor(authorId);
+    }
+
+    @PostMapping("/posts")
+    public String createPost(@RequestBody Post post, @RequestParam int authorId) {
+        return postService.createPost(post, authorId);
+    }
+
+    @PutMapping("/posts")
+    public String editPost(@RequestBody Post post) {
+        return postService.editPost(post);
+    }
+
+    @DeleteMapping("posts/{postId}")
+    public String deletePost(@PathVariable int postId) {
+        return postService.deletePost(postId);
+    }
 }
--- src/main/java/com/emeka/blogspringboot/services/PostService.java
+++ src/main/java/com/emeka/blogspringboot/services/PostService.java
@@ -1,7 +1,51 @@
 package com.emeka.blogspringboot.services;
 
+import com.emeka.blogspringboot.ResourceNotFoundException;
+import com.emeka.blogspringboot.models.Author;
+import com.emeka.blogspringboot.models.Post;
+import com.emeka.blogspringboot.repositories.AuthorRepository;
+import com.emeka.blogspringboot.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public List<Post> getPostsByAuthor(int authorId) {
+        return postRepository.findByAuthorId(authorId);
+    }
+
+    public String createPost(Post post, int authorId)  {
+        Author author = authorRepository.findById(authorId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("AuthorId %s not found", authorId)));
+        post.setAuthor(author);
+        postRepository.save(post);
+        return "Post was created successfully";
+    }
+
+    public String editPost(Post post) {
+        Post thePost = postRepository.findById(post.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Post not found"));
+        thePost.setBody(post.getBody());
+        thePost.setTitle(post.getTitle());
+        postRepository.save(thePost);
+
+        return "Post was edited successfully";
+    }
+
+    public String deletePost(int postId) {
+        postRepository.deleteById(postId);
+        return "Post deleted successfully";
+    }
 }
